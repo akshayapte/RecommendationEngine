e446ac9a984609e7a7e59987fb290bc61191760e
akshaykolte <akki.kolte@gmail.com>
2016-03-17 01:59:18 +0530

    Adding users

--- recommender/recommender_app/dummy_db.py
+++ recommender/recommender_app/dummy_db.py
@@ -1,5 +1,19 @@
 from recommender_app.models import *
-import random
+from random import randrange
+from datetime import timedelta
+from itertools import combinations
+from time import sleep
+import sys, math, random, datetime
+import string
+def random_date(start, end):
+    """
+    This function will return a random datetime between two datetime 
+    objects.
+    """
+    delta = end - start
+    int_delta = (delta.days * 24 * 60 * 60) + delta.seconds
+    random_second = randrange(int_delta)
+    return start + timedelta(seconds=random_second)
 
 def populate():
     brand_names = ['Nike', 'Adidas', 'Puma', 'Reebok', 'Alley Solly', 'Peter England', 'Louis Philippe', 'Abercrombie', 'Roadster', 'Max', 'FCUK', 'Provogue']
@@ -59,3 +73,44 @@ def populate():
                         gender=l_gender, brand=l_brand, neckline=l_neckline,
                         sleeves=l_sleeves, fit=l_fit, price=l_price)
         g_obj.save()
+
+
+def insert_users(n=105):
+    print "Adding Users...",
+    n+=1
+    f = open("dummy_db/students.txt", "r+")
+    stu_list = f.readlines()
+    length = n-1
+    for i,stu in enumerate(stu_list):
+        n-=1;
+        if n <= 0:
+            break
+        line = stu.split('$')
+        username = line[0]
+        password = line[1]
+        first_name = line[2]
+        last_name = line[3]
+        address = line[4]
+        email = line[5]
+        phone_number = line[6]
+        gender = line[7].rstrip('\n')
+        if not User.objects.filter(name=first_name+last_name).exists():
+            user_obj = User(name=first_name+last_name, email=email, phone_number=phone_number, gender=gender, birth_date = models.DateField())
+            user_obj.save()
+    f.close()
+    print ""
+
+
+def main():
+    d1 = datetime.strptime('1/1/1974 1:30 PM', '%m/%d/%Y %I:%M %p')
+    d2 = datetime.strptime('1/1/1995 4:50 AM', '%m/%d/%Y %I:%M %p')
+
+    print random_date(d1, d2)    
+    user_obj = User(name=first_name+last_name, email=email, phone_number=phone_number, gender=gender, birth_date = random_date(d1, d2))
+    user_obj.save()
+
+
+
+if __name__ == "__main__":
+    print "hrre"
+    main()        
